53689e84d89e3fe58141bd3816ee2af12038e580
Clay <hellpigeon01@gmail.com>
2024-07-15 15:44:28 -0400

    Setting up Svelte

---
+++ src/components/slider.svelte
@@ -0,0 +1,9 @@
+<script>
+    
+</script>
+
+<h1>This is the Slider Component.</h1>
+
+<style>
+    
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import './normalize.css';
+</script>
+
+<div class="container"><slot /></div>
+
+<style>
+	.container {
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 0 20px;
+		border: 1px solid #000;
+	}
+</style>
